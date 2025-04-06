d0e6ab4a9907772242e4a8e052134d945d787240
Mohamad <ghost4ft@gmail.com>
2025-04-06 20:52:54 +0330

    useState for save last search

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,39 @@
 import Search from "@/components/Search";
 import Categories from "@/components/Categories";
 import Api from "@/utils/db";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const db = Api
-  const [search, setSearch] = useState("")
+  const db = Api;
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const savedSearch = localStorage.getItem("search");
+      if (savedSearch) {
+        setSearch(savedSearch);
+      }
+    }
+  }, []);
+
+  // Save search to localStorage whenever it changes
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem("search", search);
+    }
+  }, [search]);
+
   const onListener = (e) => {
     setSearch(e.target.value);
-  }
+  };
+
   const filteredData = db.filter((item) =>
-    (item.title.toLowerCase().includes(search.toLowerCase()))
-  )
+    item.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="container mx-auto px-4 flex flex-col h-screen items-center justify-around">
-      <Search onSearch={onListener} />
+      <Search onSearch={onListener} lastSearch={search} />
       <Categories key={db.id} data={filteredData} />
     </div>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,11 @@
 "use client";
-export default function Search({ onSearch }) {
+export default function Search({ onSearch, lastSearch }) {
   return (
     <div className="container mx-auto my-4 flex flex-row items-center justify-center">
       <input
         type="text"
         placeholder="Search..."
+        value={lastSearch}
         onChange={(e) => {
           onSearch(e);
         }}
